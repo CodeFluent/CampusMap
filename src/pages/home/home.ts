d053a5b645129a7c868aa322574d2a44e260e7ca
CodeFluent <wasfimomenwim@gmail.com>
2018-04-23 12:56:05 -0400

    added map

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import leaflet from 'leaflet';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild('map') mapContainer: ElementRef;
+  map: any;
 
   drawerOptions: any;
 
@@ -16,6 +19,18 @@ export class HomePage {
       thresholdFromTop: 200,
       bounceBack: true
     };
+  }
+
+  ionViewDidEnter() {
+    this.loadMap();
+  }
+
+  loadMap() {
+    this.map = leaflet.map("map").fitWorld();
+    leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      center: [33.753068, -84.385282]
+    }).addTo(this.map);
   }
 
 }
