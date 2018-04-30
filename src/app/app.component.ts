f115cbb81860cac60061dcbc6128d1d57dde4353
CodeFluent <wasfimomenwim@gmail.com>
2018-04-30 19:03:15 -0400

    reformatted project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,66 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
 import { HomePage } from '../pages/home/home';
+import { FeedPage } from '../pages/feed/feed';
+import { SubscribePage } from '../pages/subscribe/subscribe';
+
+import { LogoutPage } from '../pages/logout/logout';
+import { LoginPage } from '../pages/login/login';
+import { SignupPage } from '../pages/signup/signup';
+
+
+import { UserDataProvider } from '../providers/user-data';
+
+export interface PageInterface {
+  title: string;
+  name: string;
+  component: any;
+  icon: string;
+  index?: number;
+  tabName?: string;
+  tabComponent?: any;
+
+}
 
 @Component({
   templateUrl: 'app.html'
 })
-export class MyApp {
+export class CampusMap {
+  @ViewChild(Nav) nav: Nav;
+
   rootPage: any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
+  allPages: PageInterface[] = [
+    { title: 'Main Map', name: 'Mainmap', component: TabsPage, tabComponent: HomePage, index: 0, icon: 'map' },
+    { title: 'Feeds', name: 'FeedPage', component: TabsPage, tabComponent: FeedPage, index: 1, icon: 'paper' },
+    { title: 'Subscriptions', name: 'SubscribePage', component: TabsPage, tabComponent: SubscribePage, index: 2, icon: 'settings' }
+  ];
+  loggedInPages: PageInterface[] = [
+    { title: 'Logout', name: 'LogoutPage', component: LogoutPage, icon: 'log-out' }
+  ];
+  loggedOutPages: PageInterface[] = [
+    { title: 'Login', name: 'LoginPage', component: LoginPage, icon: 'log-in' },
+    { title: 'Signup', name: 'SignupPage', component: SignupPage, icon: 'person-add' }
+  ];
+
+  constructor(
+    public platform: Platform,
+    public statusBar: StatusBar,
+    public splashScreen: SplashScreen) {
+
+    this.platformReady();
+  }
+
+  platformReady() {
+    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
-import { MyApp } from './app.component';
+import { CampusMap } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { FeedPage } from '../pages/feed/feed';
+
+import { LogoutPage } from '../pages/logout/logout';
+
+
 import { SubscribePage } from '../pages/subscribe/subscribe';
 import { FeedDataProvider } from '../providers/feed-data';
 import { DatasetsProvider } from '../providers/datasets';
@@ -15,7 +19,7 @@ import { UserDataProvider } from '../providers/user-data';
 
 @NgModule({
   declarations: [
-    MyApp,
+    CampusMap,
     HomePage,
     FeedPage,
     SubscribePage,
@@ -23,11 +27,18 @@ import { UserDataProvider } from '../providers/user-data';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(CampusMap, {}, {
+      links: [
+        { component: TabsPage, name: 'TabsPage', segment: 'tabs-page' },
+        { component: HomePage, name: 'Mainmap', segment: 'main-map' },
+        { component: FeedPage, name: 'FeedPage', segment: 'feeds' },
+        { component: SubscribePage, name: 'SubscribePage', segment: 'subscriptions' }
+      ]
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
+    CampusMap,
     FeedPage,
     HomePage,
     SubscribePage,
